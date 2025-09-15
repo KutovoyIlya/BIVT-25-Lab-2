a8cb49d6d6edd7f7b9592d964e99f81ffce753ec
RuGilf <hackjackbrain@gmail.com>
2025-09-15 10:06:43 +0300

    Update Purple.cs

--- Lab2/Purple.cs
+++ Lab2/Purple.cs
@@ -78,20 +78,6 @@ namespace Lab2
             long answer = 0;
             long zerna = 2;
 
-            for (int i = 0; i != 62; i++)
-            {
-                zerna *= 2;
-            }
-
-            answer = zerna / (15 * 1000 * 1000);
-            return answer;
-        }
-
-        public long Task6()
-        {
-            long answer = 0;
-            long zerna = 2;
-
             for (int i = 0; i != 57; i++)
             {
                 zerna *= 2;
@@ -99,6 +85,25 @@ namespace Lab2
 
             answer = zerna / (15 * 125 * 125);
             return answer;
+        }
+
+        public int Task7(double S, double d)
+        {
+            double Ss = S;
+            int n = 0;
+            double YP = 0;
+            
+            while (Ss < 2 * S)
+            {
+                if (n == 0 || n % 12 == 0)
+                {
+                    YP = Ss * d;
+                }
+                Ss += YP / 2;
+                n++;
+            }
+
+            return n;
         }
         public (double SS, double SY) Task8(double a, double b, double h)
         {
@@ -113,4 +118,3 @@ namespace Lab2
         }
     }
 }
-
