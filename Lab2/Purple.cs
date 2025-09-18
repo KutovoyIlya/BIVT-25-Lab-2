476b97c1bd2d0fa9f98caca836839c3bdc92ade7
CuttiePie <144522961+OtterCuttie@users.noreply.github.com>
2025-09-18 13:32:44 +0300

    сделала 1,5,6 purple

--- Lab2/Purple.cs
+++ Lab2/Purple.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Lab2
 {
@@ -12,7 +13,10 @@ namespace Lab2
             int answer = 0;
 
             // code here
-
+            for (int i = 0; i< n; i++)
+            {
+                answer += (p+h*i)* (p + h*i);
+            }
             // end
 
             return answer;
@@ -23,7 +27,18 @@ namespace Lab2
             int remainder = 0;
 
             // code here
+            if (a == b)
+            {
+                quotient = 1;
+                return (quotient, remainder);
+            }
 
+            while(a > b)
+            {
+                a -= b;
+                quotient++;
+                remainder = a;
+            } 
             // end
 
             return (quotient, remainder);
@@ -51,9 +66,18 @@ namespace Lab2
         public int Task5(int a, int b)
         {
             int answer = 0;
-
+            long number = a;
             // code here
-
+            while (b > 0)
+            {
+                number *= b;
+                b --;
+            }
+            while (number >= 10)
+            {
+                number /= 10;
+                answer++;
+            }
             // end
 
             return answer;
@@ -63,7 +87,15 @@ namespace Lab2
             long answer = 0;
 
             // code here
-
+            int a = 1;
+            decimal b= 1;
+            while (a <= 64)
+            {
+                a++;
+                b *= 2;
+            }
+            decimal d = (b /15)/ 1000000;
+            answer = (long)Convert.ToInt64(Math.Floor(d));
             // end
 
             return answer;
@@ -72,9 +104,21 @@ namespace Lab2
         public int Task7(double S, double d)
         {
             int answer = 0;
-
             // code here
+            int counter(double S, double d) { }
+            {
 
+            }
+            double S2 = S * 2;
+            while (S == S2)
+            {
+
+            }
+            {
+
+            }
+
+            
             // end
 
             return answer;
