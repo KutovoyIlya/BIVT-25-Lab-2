883efc24971ca65fd20b128833304936a5dcdc3d
neemilic <118543701+neemilic@users.noreply.github.com>
2025-09-27 22:48:49 +0300

    Update Purple.cs

--- Lab2/Purple.cs
+++ Lab2/Purple.cs
@@ -1,5 +1,6 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data.SqlTypes;
 using System.Runtime.InteropServices;
 
 namespace Lab2
@@ -10,10 +11,10 @@ namespace Lab2
         public int Task1(int n, int p, int h)
         {
             int answer = 0;
-
-            // code here
-
-            // end
+            for (int i = 0; i < n; i++)
+            {
+                answer += (p + h * i) * (p + h * i);
+            }
 
             return answer;
         }
@@ -21,20 +22,37 @@ namespace Lab2
         {
             int quotient = 0;
             int remainder = 0;
+            int i = 0;
 
-            // code here
-
-            // end
-
+            while (a - b >= 0)
+            {
+                quotient++;
+                a -= b;
+            }
+            remainder = a;
             return (quotient, remainder);
         }
         public double Task3()
         {
             double answer = 0;
 
-            // code here
-
-            // end
+            const double e = 0.0001;
+            double old_num = 2;
+            double old_den = 1;
+            double new_num = 3;
+            double new_den = 2;
+            while (true)
+            {
+                new_num = old_num + new_num;
+                new_den = old_den + new_den;
+                old_num = new_num - old_num;
+                old_den = new_den - old_den;
+                if (Math.Abs((new_num / new_den) - (old_num / old_den)) < e)
+                {
+                    answer = (new_num / new_den);
+                    break;
+                }
+            }
 
             return answer;
         }
@@ -42,52 +60,97 @@ namespace Lab2
         {
             int answer = 0;
 
-            // code here
-
-            // end
-
+            const double e = (double)1 / 10000;
+            int i = 1;
+            while (Math.Abs(b) >= e)
+            {
+                i++;
+                b = b * q;
+            }
+            answer = i;
             return answer;
         }
         public int Task5(int a, int b)
         {
             int answer = 0;
+            long number = a;
 
-            // code here
-
-            // end
-
+            while (b > 0)
+            {
+                number *= b;
+                b--;
+            }
+            while (number >= 10)
+            {
+                number /= 10;
+                answer++;
+            }
             return answer;
         }
         public long Task6()
         {
             long answer = 0;
+            ulong amount = 0;
+            ulong cur = 1;
+            for (int i = 1; i < 65; i++)
+            {
 
-            // code here
-
-            // end
-
+                amount += cur;
+                cur *= 2;
+            }
+            amount = amount / 15000000;
+            answer = (long) amount;
             return answer;
         }
 
         public int Task7(double S, double d)
         {
             int answer = 0;
+            double s = S * 1000;
+            double sn = s;
+            double r = d / 100;
+            double sk = s;
 
-            // code here
-
-            // end
-
+            for (int i = 1; i < 12000; i++)
+            {
+                if (i % 12 == 1)
+                {
+                    sn = (sk * r / 12);
+                }
+                sk += sn;
+                if (sk > 2 * s)
+                {
+                    answer = i;
+                    break;
+                }
+            }
             return answer;
         }
         public (double SS, double SY) Task8(double a, double b, double h)
         {
             double SS = 0;
             double SY = 0;
-
-            // code here
-
-            // end
-
+            double e = 0.0001;
+            static long Factorial(int n)
+            {
+                long res = 1;
+                for (int i = 1; i <= n; i++)
+                {
+                    res *= i;
+                }
+                return res;
+            }
+            for (double x = a; x <= b + e; x+=h)
+            {
+                SY += Math.Cos(x);
+                for (int i = 0; i < 100; i++)
+                {
+                    double SX  = Math.Pow(-1, i) * Math.Pow(x, 2*i) / Factorial(2*i);
+                    SS += SX;
+                    if (Math.Abs(SX) < e)
+                        break;
+                }      
+            }
             return (SS, SY);
         }
     }
