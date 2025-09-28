bfe0dacdbbc5f6ad1b4c808780a18ba7f0f500fc
krisp-bit <yerokhina08@list.ru>
2025-09-28 21:54:10 +0300

    Update Green.cs

--- Lab2/Green.cs
+++ Lab2/Green.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
+using System.Security.Cryptography;
 
 namespace Lab2
 {
@@ -11,7 +12,10 @@ namespace Lab2
             double answer = 0;
 
             // code here
-
+            for (int i = 2; i <= n; i += 2)
+            {
+                answer += (double) i / (i + 1);
+            }
             // end
 
             return answer;
@@ -21,17 +25,30 @@ namespace Lab2
             double answer = 0;
 
             // code here
+            double xx = x;
+            for (int i = 1; i <= n; i++)
+            {
+                answer += 1 + (1 / xx);
+                xx = x * xx;
 
+            }
             // end
 
             return answer;
         }
         public long Task3(int n)
         {
-            long answer = 0;
+            long answer = 1;
 
             // code here
-
+            for (int i = 0; i <= n; i ++)
+            {
+                if (i > 0)
+                {
+                    answer *= i;
+                }
+                
+            }
             // end
 
             return answer;
@@ -39,9 +56,20 @@ namespace Lab2
         public double Task4(double x)
         {
             double answer = 0;
-
+            
             // code here
+            if (Math.Abs(x) < 1)
+            {
+                int n = 4;
+                double xx = x;
+                for (int i = 1; i < n; i++)
+                {
+                    answer += Math.Sin(xx);
+                    xx *= x;
+                    xx = xx * i + 1;
 
+                }
+            }
             // end
 
             return answer;
@@ -51,17 +79,36 @@ namespace Lab2
             int answer = 0;
 
             // code here
-
+            if (Math.Abs(x) > 1)
+            {
+                int n = 1;
+                while (true)
+                {
+                    double chi1 = 1 / Math.Pow(x, n);
+                    double chi2 = 1 / Math.Pow(x, n - 1);
+                    if (chi1 - chi2 < E)
+                    {
+                        answer = n; break;
+                    }
+                    n ++;
+                }
+            }
             // end
 
-            return answer;
+                return answer;
         }
         public int Task6(int limit)
         {
             int answer = 0;
 
             // code here
-
+            int elem = 1, i = 0;
+           while (elem < limit)
+            {
+                elem *= 2;
+                answer += elem;
+                i++;
+            }
             // end
 
             return answer;
@@ -70,9 +117,13 @@ namespace Lab2
         public int Task7(double L)
         {
             int answer = 0;
-
             // code here
+            while (L > Da)
+            {
+                L /= 2;
+                answer++;
 
+            }
             // end
 
             return answer;
