48cb10dec31d326ebf7195190642277884589194
Kisluhina-Ekaterina <kisluhinaekaterina12@gmail.com>
2025-09-28 18:51:48 +0300

    Add files via upload

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -4,7 +4,15 @@
     {
         public static void Main()
         {
-
+            White white = new White();
+            //Console.WriteLine(white.Task1(5));
+            //Console.WriteLine(white.Task2(5));
+            //Console.WriteLine(white.Task3(3));
+            //Console.WriteLine(white.Task4(2, 5));
+            //Console.WriteLine(white.Task5(500));
+            //Console.WriteLine(white.Task6(0.6));
+            //Console.WriteLine(white.Task7());
+            Console.WriteLine(white.Task8(280, 30));
         }
     }
 }
--- Lab2/White.cs
+++ Lab2/White.cs
@@ -8,7 +8,10 @@
             int answer = 0;
 
             // code here
-
+            int a1 = 2;
+            int a9 = 3 * n - 1;
+            answer = ((a1 + a9) * n) / 2;
+            
             // end
 
             return answer;
@@ -18,27 +21,37 @@
             double answer = 0;
 
             // code here
-
+            double a = 1;
+            for (int b = 1; b <= n; b ++)
+            {
+                answer += a / b;
+            }
             // end
 
             return answer;
         }
         public long Task3(int n)
         {
-            long answer = 0;
+            long answer = 1;
 
             // code here
-
+            for (int a = n; a > 1; a--)
+                answer *= a;
             // end
 
             return answer;
         }
         public long Task4(int a, int b)
         {
-            long answer = 0;
+            long answer = 1;
 
             // code here
-
+           
+            for(int i = 1; i <= b; i ++)
+            {
+                answer *= a;
+            }
+            
             // end
 
             return answer;
@@ -48,7 +61,13 @@
             int answer = 0;
 
             // code here
-
+            int n = 1, p = 1;
+            while(p <= L)
+            {
+                n += 3;
+                p *= n;
+                answer = n;
+            }
             // end
 
             return answer;
@@ -57,32 +76,63 @@
         {
             double answer = 0;
 
-            // code here
+            //code here
+            double s = 0;
+            
+            int n = 0; 
+            do
+            { 
+            
+                double r = Math.Pow(x, 2 * n);
+                n += 1;
+                s += r;
+                if (r <= 0.0001)
+                    break;
+                Console.WriteLine(r);
 
-            // end
+                
+            }
+            while (true);
+            answer = s;
+            //end
 
             return answer;
+
         }
+
 
         public int Task7(int n)
         {
             int answer = 0;
 
             // code here
-
+            int sum = 0;
+            while (sum < n)
+            {
+                answer++;
+                sum += answer;
+            }
+            
+           
             // end
 
             return answer;
         }
-        public int Task8(double L, double v)
+        public int Task8(double L, double V)
         {
             int answer = 0;
             const double R = 6371.0; // радиус Земли, км
 
             // code here
+            
+            double X = Math.Sqrt(Math.Pow(R, 2) + Math.Pow(L, 2));
+            double Y = ((X - R) / V)+1;
+            double a = Math.Floor(Y);
 
+            
             // end
-
+            Console.WriteLine(Y);
+            answer = (int)a;
             return answer;
         }
     }
