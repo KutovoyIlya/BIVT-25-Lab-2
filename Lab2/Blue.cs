a431e20e501a331dea6882f240aa0b410f862fe9
liza-127 <lizadaystar12737@gmail.com>
2025-09-29 21:46:17 +0300

    Update Blue.cs

--- Lab2/Blue.cs
+++ Lab2/Blue.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
 
@@ -10,18 +10,30 @@ namespace Lab2
         public double Task1(int n, double x)
         {
             double answer = 0;
+            double m = 1;
 
             // code here
+            for (int i = 1; i <= n; i++)
+            {
+                answer += (Math.Sin(x * i) / m);
+                m = m * x;
+                
+            }
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
         public double Task2(int n)
         {
             double answer = 0;
 
             // code here
+            double x = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                answer += (Math.Pow((-1), i) * (Math.Pow(5, i) / x));
+                x *= (i + 1);
+            }
 
             // end
 
@@ -32,7 +44,23 @@ namespace Lab2
             long answer = 0;
 
             // code here
-
+            long a1 = 1;
+            long a2 = 0;
+            long s = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (i == 0)
+                {
+                    s= 0;
+                }
+                else
+                {
+                    s  = a1 + a2;
+                    a1 = a2;
+                    a2 = s;
+                }
+                answer += s;
+            }
             // end
 
             return answer;
@@ -42,7 +70,20 @@ namespace Lab2
             int answer = 0;
 
             // code here
-
+            int n = 1;
+            int s = 0;
+            do
+            {
+                s += (a + (n - 1) * h);
+                n++;
+                answer++;
+                Console.WriteLine(s);
+            }
+            while (s < L);
+            if (s > L)
+            {
+                answer -= 1;
+            }
             // end
 
             return answer;
@@ -52,7 +93,19 @@ namespace Lab2
             double answer = 0;
 
             // code here
-
+            double ch = 0;
+            double zn = 1;
+            double elem = ch / zn;
+            int i = 1;
+            do
+            {
+                ch += i;
+                zn *= x;
+                answer += elem;
+                elem = ch / zn;
+                i += 1;
+            }
+            while (elem > 0.0001);
             // end
 
             return answer;
@@ -62,7 +115,12 @@ namespace Lab2
             int answer = 0;
 
             // code here
-
+            do
+            {
+                answer += h;
+                S *= 2;
+            }
+            while (S < L);
             // end
 
             return answer;
@@ -74,7 +132,28 @@ namespace Lab2
             int c = 0;
 
             // code here
+            double s = 0;
+            double s1 = S;
+            double s2 = S;
+            double s3 = 0;
+            for (int i = 1; i <= 7; i += 1)
+            {
+                a += S;
+                S += (S * (I / 100));
+            }
+            while (s < 100)
+            {
+                b += 1;
+                s += s1;
+                s1 += (s1 * (I / 100));
 
+            }
+            while (s2 <= 42)
+            {
+                c += 1;
+                s2 += (s2 * (I / 100));
+
+            }
             // end
 
             return (a, b, c);
@@ -85,10 +164,32 @@ namespace Lab2
             double SY = 0;
 
             // code here
+            double x = a;
+            while (x <= b + 1e-10)
+            {
+                double x2 = 1;
+                int i = 0;
+                int i2 = 1;
+                double s = 0;
 
-            // end
+                do
+                {
+                    if (i > 0)
+                    {
+                        i2 *= i;
+                    }
+                    SS += ((2 * i + 1) * x2) / i2;
+                    x2 *= x * x;
+                    i++;
+                }
+                while (Math.Abs(((2 * i + 1) * x2) / i2) >= E);
 
-            return (SS, SY);
+                SY += (1 + 2 * x * x) * Math.Exp(x * x);
+                x += h;
+
+                // end
+            }
+                return (SS, SY);
         }
     }
 }
