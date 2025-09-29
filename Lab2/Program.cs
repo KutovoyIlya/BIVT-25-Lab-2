0133b42ce13478f47d566028a898fcec40d4b174
Зорина Вера <verazorina71136@gmail.com>
2025-09-29 17:13:18 +0300

    Add files via upload

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -4,7 +4,14 @@
     {
         public static void Main()
         {
-
+            Green green = new Green();
+            //Console.WriteLine(white.Task1(1.2));
+            //Console.WriteLine(white.Task2(2));
+            //Console.WriteLine(white.Task3(2, 189));
+            //Console.WriteLine(white.Task4(-2.5, 1.89));
+            //Console.WriteLine(white.Task5(0.5));
+            //Console.WriteLine(green.Task7(1.01));
+            Console.WriteLine(green.Task8(0.001, 0.1, 0.001));
         }
     }
 }
