cf84ae421336b7d2a469521f8a9a74567eb6d72a
demid <76248933+demid712@users.noreply.github.com>
2025-09-29 13:13:41 +0300

    Add files via upload

--- Lab2/Blue.cs
+++ Lab2/Blue.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq.Expressions;
 using System.Runtime.InteropServices;
 
 namespace Lab2
@@ -12,7 +13,13 @@ namespace Lab2
             double answer = 0;
 
             // code here
-
+            double pw = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                answer += Math.Sin(x * i) / pw;
+                pw *= x;
+            }
+            
             // end
 
             return answer;
@@ -22,6 +29,16 @@ namespace Lab2
             double answer = 0;
 
             // code here
+            double pw1= 5;
+            double pw2 = 1;
+            int x = -1;
+            for (int i = 1; i <= n; i++)
+            {
+                answer += x * pw1 / pw2;
+                pw1 *= 5;
+                x *= -1;
+                pw2 *= i+1;
+            }
 
             // end
 
@@ -32,7 +49,16 @@ namespace Lab2
             long answer = 0;
 
             // code here
+            long now = 1, cnt=1;
+            long pre = 0; 
+            for (int i = 0; i < n-1; i++)
+            {
+                answer+= now;
+                cnt = now;
+                now += pre;
+                pre = cnt;
 
+            }
             // end
 
             return answer;
@@ -42,7 +68,12 @@ namespace Lab2
             int answer = 0;
 
             // code here
-
+            int n = 0, cnt = 0;
+            while (cnt <= L)
+            {
+                answer = n++;
+                cnt += a + (n - 1) * h;
+            }
             // end
 
             return answer;
@@ -52,6 +83,17 @@ namespace Lab2
             double answer = 0;
 
             // code here
+            double ch = 0, zn = 1, elem = ch / zn;
+            int i = 1;
+            do
+            {
+                ch += i;
+                zn *= x;
+                answer += elem;
+                elem = ch / zn;
+                i++;
+            }
+            while (elem > 0.0001);
 
             // end
 
@@ -62,7 +104,11 @@ namespace Lab2
             int answer = 0;
 
             // code here
-
+            while (L > S)
+            {
+                answer += h;
+                S *= 2;
+            }
             // end
 
             return answer;
@@ -74,7 +120,30 @@ namespace Lab2
             int c = 0;
 
             // code here
-
+            double Sa = S, Ia = I, Ssuma=0;
+            for (int i=0; i<7; i++)
+            {
+                Ssuma += Sa;
+                Sa *= (Ia / 100 + 1);
+            }
+            a = Ssuma;
+            double Sb = S, Ib = I, Ssumb=0;
+            int ib = 0;
+            while (Ssumb < 100)
+            {
+                Ssumb += Sb;
+                Sb*= (Ib/100 +1);
+                ib++;
+            }
+            b = ib;
+            double Sc = S, Ic = I;
+            int ic = 0;
+            while (Sc <= 42)
+            {
+                Sc *= (Ic / 100 + 1);
+                ic++;
+            }
+            c = ic;
             // end
 
             return (a, b, c);
